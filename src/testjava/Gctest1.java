0e60245b4ea7e317765cbca1dcbde59d8b2a1022
gacch <gacch@gacch-PC>
2015-12-12 18:57:35 +0900

    s

---
+++ src/testjava/Gctest1.java
@@ -0,0 +1,13 @@
+package testjava;
+
+public class Gctest1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		while(true){
+			String s = new String("abxdefgahahahahahahhahahhaha");
+			System.out.println(Runtime.getRuntime().freeMemory());
+		}
+	}
+
+}
